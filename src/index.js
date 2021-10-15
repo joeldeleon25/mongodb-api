db7c1bd36e867dcdc401ea898949d86653c94c49
Joel Deleon <joelmartinezdeleon96@gmail.com>
2021-10-14 22:26:13 -0600

    Upload API project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express  = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./routes/user");
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middleware
+app.use(express.json());
+app.use('/api', userRoutes);
+
+
+//routes
+app.get("/" , (req, res) => {
+    res.send("welcome to my Api");
+});
+
+//mongodb
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log("Connected to ATLAS"))
+.catch((error) => console.error(error));
+
+app.listen(port, () => console.log('servicio listado en ' , port));
+
+
